e206d93c3f5252a3814756424aa3d78abd18f764
David Kljajo <davidkljajo@gmail.com>
2023-03-21 15:02:15 +0100

    Adding week 3

---
+++ week-3 scripts/skripta30.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# testing 2 commands line params
+
+total=$[ $1 * $2 ]
+echo The first param is $1.
+echo The second param is $2.
+echo The total Value is $total.
+
+
+
+
---
+++ week-3 scripts/skripta33.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# testing the $0 param
+
+
+echo The zero parameter is set to : $0
+
+
+
