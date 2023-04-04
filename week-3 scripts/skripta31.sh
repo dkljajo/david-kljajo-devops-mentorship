ba43b09abbd2834b725ba22595c17babef465ca7
David Kljajo <davidkljajo@gmail.com>
2023-04-04 18:30:17 +0200

    Delete week-3 scripts directory

--- week-3 scripts/skripta31.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# testing string params
-
-
-echo Hello $1, glad to meet you.
-
-
-
-
--- week-3 scripts/skripta33.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# testing the $0 param
-
-
-echo The zero parameter is set to : $0
-
-
-
